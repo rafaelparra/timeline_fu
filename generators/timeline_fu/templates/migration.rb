8fedd3b1b62b7144055b24d218c9f57ab4e0d88d
rafaelparra <eduardopa@gmail.com>
2012-09-24 12:07:58 -0700

    store json of subject in event record

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -3,8 +3,15 @@ class CreateTimelineEvents < ActiveRecord::Migration
     create_table :timeline_events do |t|
       t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type
       t.integer               :subject_id,    :actor_id,    :secondary_subject_id
+      t.text     :stored_subject
       t.timestamps
     end
+    add_index :timeline_events, :subject_type
+    add_index :timeline_events, :actor_type
+    add_index :timeline_events, :secondary_subject_type
+    add_index :timeline_events, :subject_id
+    add_index :timeline_events, :actor_id
+    add_index :timeline_events, :secondary_subject_id
   end
  
   def self.down
--- generators/timeline_fu/templates/model.rb
+++ generators/timeline_fu/templates/model.rb
@@ -2,4 +2,11 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :actor,              :polymorphic => true
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
+  
+  validates_presence_of :subject_type, :subject_id
+  
+  def json_subject
+    ActiveSupport::JSON.decode(stored_subject)["#{subject_type.underscore}"] if stored_subject
+  end
+  
 end
