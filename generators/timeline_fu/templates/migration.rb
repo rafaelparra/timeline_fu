86e78a9b38b3e6de85588831356aa351dbca7bed
rafaelparra <eduardopa@gmail.com>
2012-10-05 16:45:32 -0700

    context

--- generators/timeline_fu/templates/migration.rb
+++ generators/timeline_fu/templates/migration.rb
@@ -1,17 +1,21 @@
 class CreateTimelineEvents < ActiveRecord::Migration
   def self.up
     create_table :timeline_events do |t|
-      t.string   :event_type, :subject_type,  :actor_type,  :secondary_subject_type
-      t.integer               :subject_id,    :actor_id,    :secondary_subject_id
+      t.string   :event_type, :limit => 30
+      t.string   :subject_type, :limit => 30
+      t.string   :actor_type, :limit => 30
+      t.string   :secondary_subject_type, :limit => 30
+      t.string   :context_type, :limit => 30
+      t.integer  :subject_id,    :actor_id,    :secondary_subject_id
       t.text     :stored_subject
       t.timestamps
     end
-    add_index :timeline_events, :subject_type
+    add_index :timeline_events, [:actor_id, :secondary_subject_id], :name => 'chalkboard_index'
     add_index :timeline_events, :actor_type
     add_index :timeline_events, :secondary_subject_type
-    add_index :timeline_events, :subject_id
-    add_index :timeline_events, :actor_id
-    add_index :timeline_events, :secondary_subject_id
+    add_index :timeline_events, [:subject_id, :subject_type], :name => 'subject_index'
+    add_index :timeline_events, [:context_id, :context_type], :name => 'context_index'
+    add_index :timeline_events, :created_at
   end
  
   def self.down
--- generators/timeline_fu/templates/model.rb
+++ generators/timeline_fu/templates/model.rb
@@ -3,6 +3,7 @@ class TimelineEvent < ActiveRecord::Base
   belongs_to :actor,              :polymorphic => true
   belongs_to :subject,            :polymorphic => true
   belongs_to :secondary_subject,  :polymorphic => true
+  belongs_to :context,            :polymorphic => true
   
   validates_presence_of :subject_type, :subject_id
   
